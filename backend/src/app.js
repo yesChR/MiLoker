ae05bbd37bcfe0a47cc3aaf966483c7a60440c1d
yesChR <yeschinchilla22@gmail.com>
2025-05-24 21:10:57 -0600

    Ajuste en el archivo app por incompatibilidad

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,12 @@ import config from './config/config';
 
 const nodemailer = require("./routes/nodemailer.routes");
 const administrador= require("./routes/Administrativo/administrador.routes");
+const profesor = require("./routes/administrativo/profesor.routes");
+const estudiante = require("./routes/administrativo/estudiante.routes");
+const especialidad = require("./routes/administrativo/especialidad.routes");
+const sancion = require("./routes/administrativo/sancion.routes");
+import casillero from "./routes/casillero/casillero.routes.js";
+
 
 const app = express();
 const cors = require("cors");
@@ -18,5 +24,11 @@ app.use(express.json());
 // Rutas (Agregar todas las rutas que existan en Routes)
 app.use("/nodemailer", nodemailer);
 app.use("/administrador", administrador);
+
+app.use("/profesor", profesor);
+app.use("/estudiante", estudiante);
+app.use("/especialidad", especialidad);
+app.use("/sancion", sancion);
+app.use("/casillero", casillero);
 
 export default app;
