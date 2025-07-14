08566c46f65b2ef0f71f8fe4019d2cd19474ceff
Roger Calderon <ROGER.CALDERON@ucr.ac.cr>
2025-07-14 13:36:04 -0600

    Añadir ToastProvider para gestión de notificaciones en la aplicación

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-import { HeroUIProvider } from "@heroui/react";
+import { HeroUIProvider, ToastProvider } from "@heroui/react";
 import Layout from "@/components/Layout/Layout";
 import { useRouter } from "next/router";
 
@@ -11,6 +11,7 @@ export default function App({ Component, pageProps }) {
 
   return (
     <HeroUIProvider>
+      <ToastProvider placement={"top-right"} toastOffset={placement.includes("top") ? 60 : 0} />
       {noLayoutPages.includes(router.pathname) ? (
         <Component {...pageProps} />
       ) : (
