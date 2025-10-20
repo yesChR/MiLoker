1c06ad40922c012fd9e662b6cb3d48072bd74fb7
rogerCalderon18 <calderonsteven80@gmail.com>
2025-10-20 14:07:00 -0600

    Actualizar URLs en la configuración del entorno y ajustar patrones remotos para imágenes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,24 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  allowedDevOrigins: [
-    'http://localhost:3000',
-    'http://192.168.1.254:3000',
-  ],
+  experimental: {
+    allowedDevOrigins: [
+      'http://localhost:3000',
+      'http://192.168.1.254:3000',
+    ],
+  },
   images: {
     // Configuración moderna para dominios remotos
     remotePatterns: [
       {
         protocol: 'http',
         hostname: 'localhost',
+        port: '4000',
       },
       {
         protocol: 'http',
         hostname: '127.0.0.1',
+        port: '4000',
       },
       {
         protocol: 'http',
-        hostname: '192.168.1.254',
+        hostname: 'api.miloker.ctpliv',
       },
     ],
     // Formatos soportados
